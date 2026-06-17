db67e290708fd8a4ba122e6dc772b71e2d07e2fd
Naman Crist Vatsa <namancristvats@Namans-MacBook-Air.local>
2026-06-17 20:32:01 +0530

    Adding single non repating element solution

---
+++ com.dsa.Array/SingleNonRepeatingElement.java
@@ -0,0 +1,9 @@
+class SingleNonRepeatingElement {
+    public int singleNumber(int[] nums) {
+        int single=nums[0];
+        for(int i=1;i<nums.length;i++){
+            single=single^nums[i];
+        }
+        return single;
+    }
+}
